adde225b30820ce3d247ef804d1ab32719ee04b6
Arshan Ali <96035329+arshan19@users.noreply.github.com>
2022-05-28 13:11:55 +0530

    added request and response headers

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log('andrew');
-})
-
-server.listen(4000);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project </h1></body>');
+    res.write('</html>');
+    res.end();
+});
+server.listen(3000);
