08d5a9adfc10f76dadf47638dcf31ea02e2c442f
Arshan Ali <96035329+arshan19@users.noreply.github.com>
2022-06-12 13:09:12 +0530

    Added multiple Url and fs and http request and response

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Welcome to my Node Js project </h1></body>');
-    res.write('</html>');
-    res.end();
+// Based on the url the user hits , I want you to return custom responses.
+// When url = /home , return response ==> Welcome home
+// When url = /about, return response ==> Welcome to About Us page
+// When url =/node, return response ==> Welcome to my Node Js project
+
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const server = http.createServer(function (req, res) {
+  	
+  	// localhost:9000/demo2.html 
+
+  	const urlObject = url.parse(req.url, true);
+  	const fileName = "." + urlObject.pathname;		// 		./demo2.html 	
+
+  	fs.readFile(fileName, function(err, data) {
+
+  		if (err) {
+  			res.writeHead(404, {'Content-Type': 'text/html'});
+  			return res.end("404 Not Found");
+  		}
+
+  		res.writeHead(200, {'Content-Type': 'text/html'}); 
+  		res.write(data);
+  		res.end();
+  	});
+
 });
-server.listen(3000);
+
+server.listen(3000, () => { 
+	console.log('Server is running...'); 
+}); 
